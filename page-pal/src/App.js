e6426fcfb1c4772f483255873c3b68eb7ebed3cc
KIKonstantin <kikonstantin@outlook.com>
2023-04-05 13:00:00 +0300

    softuni server updated

--- page-pal/src/App.js
+++ page-pal/src/App.js
@@ -1,11 +1,28 @@
+import { Routes, Route } from 'react-router-dom';
 
-import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Logout from './components/Logout/Logout';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <Header />
+            <div className="main-content">
+                <Routes>
+                    <Route path='/' element={<Home/>}/>
+                    <Route path='/login' element={<Login/>}/>
+                    <Route path='/logout' element={<Logout/>}/>
+                    <Route path='/register' element={<Register/>}/>
+                </Routes>
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
